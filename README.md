1bdf6be7954ac3a7401215d669b9626d013e3bf7
Marc Remijn <marc@remijn.org>
2018-01-14 16:17:48 +0100

    First version of git repo for wp-bks5-theme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bafangkids 5 wordpress theme
+
+Thema voor bafangkids site (thema 5)
+
+Gebaseerd op wordpress twentyeleven theme
+
+Inclusief een Dockerfile om op basis van een wordpress image een wordpress met dit thema te maken.
+
+
---
+++ content.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php if( has_post_thumbnail() ) { ?>
+			<!-- This adds the post thumbnail/featured image -->
+			<div class="post-thumbnail">
+				<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_post_thumbnail('excerpt-thumbnail', 'class=alignleft'); ?></a>
+			</div>
+		<?php } ?>
+		<?php if ( has_post_thumbnail() ) { ?><div class="post-wrap"><?php } ?>
+		<header class="entry-header">
+			<?php if ( is_sticky() ) : ?>
+				<hgroup>
+					<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+					<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
+				</hgroup>
+			<?php else : ?>
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<?php endif; ?>
+
+			<?php if ( 'post' == get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php twentyeleven_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php endif; ?>
+
+			<?php if ( comments_open() && ! post_password_required() ) : ?>
+			<div class="comments-link">
+				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'twentyeleven' ) . '</span>', _x( '1', 'comments number', 'twentyeleven' ), _x( '%', 'comments number', 'twentyeleven' ) ); ?>
+			</div>
+			<?php endif; ?>
+		</header> <!-- .entry-header -->
+
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+
+		<footer class="entry-meta">
+			<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- #entry-meta -->
+		<?php if ( has_post_thumbnail() ) { ?></div><?php } ?>
+	</article><!-- #post-<?php the_ID(); ?> -->
